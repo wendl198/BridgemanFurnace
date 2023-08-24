819f5cecb01585e32f2e885c84238d82a0f884be
wendl198 <124744550+wendl198@users.noreply.github.com>
2023-08-23 22:49:18 -0500

    Update SetupPi.sh

--- UsefulFiles/SetupPi.sh
+++ UsefulFiles/SetupPi.sh
@@ -2,6 +2,7 @@
 sudo apt-get install python3.9
 sudo apt-get install git
 sudo apt-get install python3-pip
+sudo apt-get install tmux
 sudo pip install Phidget22
 curl -fsSL https://www.phidgets.com/downloads/setup_linux | sudo -E bash - &&sudo apt-get install -y libphidget22
 git clone https://github.com/wendl198/BridgemanFurnace/
