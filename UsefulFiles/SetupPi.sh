a6ed85015aec3e5735698391ae83e7498d8a74bb
wendl198 <124744550+wendl198@users.noreply.github.com>
2023-08-23 22:19:55 -0500

    Create SetupPi.sh

---
+++ UsefulFiles/SetupPi.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+sudo apt-get install python3.9
+sudo apt-get install git
+sudo apt-get install python3-pip
+sudo pip install Phidget22
+curl -fsSL https://www.phidgets.com/downloads/setup_linux | sudo -E bash - &&sudo apt-get install -y libphidget22
+git clone https://github.com/wendl198/BridgemanFurnace/
