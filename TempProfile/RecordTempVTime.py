5e7610d22dff0bcfe21b141b75019e8d91c0a782
wendl198 <124744550+wendl198@users.noreply.github.com>
2023-10-12 23:36:46 -0500

    Add files via upload

--- TempProfile/RecordTempVTime.py
+++ TempProfile/RecordTempVTime.py
@@ -3,6 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import os
+from datetime import datetime
+
 segment_to_number = {
     (0, 0, 0, 0, 0, 0, 0): '',
     (1, 1, 1, 0, 1, 1, 1): '0',
@@ -27,7 +29,7 @@ def extract_text_from_frame(frame,thres):
     if dec:
         try:
             return float(segment_to_number[num1] + segment_to_number[num2] + segment_to_number[num3] + '.' + segment_to_number[num4])
-        except KeyError:
+        except (KeyError, ValueError):
             # print(num1,num2,num3,num4,dec)
             # plt.imshow(frame)
             # plt.show()
@@ -35,13 +37,12 @@ def extract_text_from_frame(frame,thres):
     else:
         try:
             return float(segment_to_number[num1] + segment_to_number[num2] + segment_to_number[num3] + segment_to_number[num4])
-        except KeyError:
-                return 0
+        except (KeyError, ValueError):
+            return 0
 
 def main(video_path):
     cap = cv2.VideoCapture(video_path)
     data = []
-    
     while cap.isOpened():
         ret, frame = cap.read()
         if not ret:
@@ -84,17 +85,20 @@ def get_pixels(img):
             output[4] = img[pixel[1]][pixel[0]]
     return tuple(output[0]),tuple(output[1]),tuple(output[2]),tuple(output[3]),output[4]
 
-folderpath = "C:/Users/blake/Downloads/"
+folderpath = "C:/Users/Contactless/Desktop/TempVideos/GrowthTemp"
 video_paths = os.listdir(folderpath)
 
-
-
 #check for success
-cap = cv2.VideoCapture(video_paths[0])
+#print(folderpath+'/'+video_paths[0])
+cap = cv2.VideoCapture(folderpath+'/'+video_paths[0])
 ret, frame = cap.read()  # Read the first frame
+
 if not ret:
-    print("Failed to read the first frame from "+ video_paths[0])
-    exit()
+    if cap.isOpened():  # Check if the video file is still open
+        print("End of video.")
+    else:
+        print("Error reading the next frame. Check the video file and format.")
+        exit()
 
 #get pixel coords
 pixel_coordinates = []
@@ -110,21 +114,26 @@ cap.release()  # Release the video capture
 
 times = [] #relative time
 temps = []
+start_times = []
 
 
 #time to collect the data from each video (this may take a while)
 for i, video_path in enumerate(video_paths):
     times.append([])
     temps.append([])
-    extracted_data = main(video_path)
+    extracted_data = main(folderpath+'/'+video_path)
     for data in extracted_data:
         times[i].append(data[0]/60)
-        temps[i].append(data[1])
-    t_data = video_path[-16:-8].split('_')
-    t_start = int(t_data[0])*60+int(t_data[1])+int(t_data[2])/60 #time in min
-    if int(t_data[0])<12:
-        t_start += 24*60
-    times[i] = np.array(times[i])+t_start#convert to absoulte time
+        temps[i].append(data[1])    
+    date_time_str = video_path.split('.')[0]
+    date_time_format = "%Y-%m-%d %H-%M-%S"# Define the format of the input string
+    date_time_obj = datetime.strptime(date_time_str, date_time_format)# Parse the input string into a datetime object
+    start_times.append(date_time_obj.timestamp())# Convert the datetime object to a Unix timestamp
+    print(date_time_str)
+    #t_data = video_path[-16:-8].split('_')
+    #t_start = int(t_data[0])*60+int(t_data[1])+int(t_data[2])/60 #time in min
+
+    times[i] = np.array(times[i])
 
     #remove outliers
     for j in range(len(temps)-5):
@@ -133,12 +142,19 @@ for i, video_path in enumerate(video_paths):
         if temps[i][j+2]<min(avg1,avg2)*.975 or temps[i][j+2]>max(avg1,avg2)*1.025:
             temps[i][j+2] = np.average([avg1,avg2])
 
-
+#save file stuff
+save_path = "C:/Users/Contactless/Desktop/TempVideos/GrowthTempData"
+save_file = open(save_path +'/'+ video_paths[0].split('.')[0] + ".dat", "a")
+save_file.write("Time (min)" + "\t" 'Temp (C)'+'\t'+"\n")
+for i in range(len(times)):
+    for j in range(len(times[i])):
+        save_file.write(times[i][j]+(start_times[i]-start_times[0])/60,temps[i][j])
+save_file.close()
 #plot temp v time
 fig1 = plt.figure(constrained_layout = True)
 ax = fig1.add_subplot(1, 1, 1)
 for i in range(len(times)):
-    ax.scatter(times[i]-times[0][0],temps[i])
+    ax.scatter(times[i]+(start_times[i]-start_times[0])/60,temps[i])
 # ax.scatter(handtime,handtemp)
 for i,path in enumerate(video_paths):
     video_paths[i] = path[len('C:/Users/blake/Downloads/WIN_20230816_'):]
